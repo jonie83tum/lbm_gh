75e84223bf67887dbccd257c788fd74554af4a17
jonie83tum <josefniedermayer@gmx.net>
2013-05-03 15:07:56 +0200

    readParameters()

--- initLB.c
+++ initLB.c
@@ -1,15 +1,27 @@
 #include "initLB.h"
 
-int readParameters(int *xlength, double *tau, double *velocityWall, int *timesteps, int *timestepsPerPlotting, int argc, char *argv[]){
-  /* TODO */
+int readParameters(int *xlength, double *tau, double *velocityWall,
+		int *timesteps, int *timestepsPerPlotting, int argc, char *argv[]) {
 
+	const char *szFileName;
+	/* Check if there is only one command line argument */
+	if (argc != 2) {
+		printf("One command line argument = file name of initialization\n");
+		return -1;
+	}
 
+	szFileName = argv[1];
+	read_int(szFileName, "xlength", xlength);
+	read_double(szFileName, "tau", tau);
+	read_double(szFileName, "velocityWall", velocityWall);
+	read_int(szFileName, "timesteps", timesteps);
+	read_int(szFileName, "timestepsPerPlotting", timestepsPerPlotting);
 
-  return 0;
+	return 0;
 }
 
-
-void initialiseFields(double *collideField, double *streamField, int *flagField, int xlength){
-  /* TODO */
+void initialiseFields(double *collideField, double *streamField, int *flagField,
+		int xlength) {
+	/* TODO */
 }
 
--- main.c
+++ main.c
@@ -7,11 +7,19 @@
 #include "visualLB.h"
 #include "boundary.h"
 
-int main(int argc, char *argv[]){
-  /* TODO */
+int main(int argc, char *argv[]) {
+	/* TODO */
+	int xlength, timesteps, timestepsPerPlotting, returnFlag;
+	double tau, velocityWall;
+	/* read the parameters */
+	returnFlag = readParameters(&xlength, &tau, &velocityWall, &timesteps,
+			&timestepsPerPlotting, argc, argv);
+	/* return in case of wrong command line arguments */
+	if (returnFlag == -1){
+		return -1;
+	}
 
-
-  return 0;
+	return 0;
 }
 
 #endif
